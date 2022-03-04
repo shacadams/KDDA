c2b1a9accd9a3a5be546308b7f526ccd30bbcd17
Shawn Adams <92461502+shacadams@users.noreply.github.com>
2022-03-04 10:51:47 -0600

    Update Linux Docker Script.sh

--- Linux Docker Script.sh
+++ Linux Docker Script.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 set -e
-# Docker CE for Linux installation script
-#
-# See https://docs.docker.com/engine/install/ for the installation steps.
-#
-# This script is meant for quick & easy install via:
-#   $ curl -fsSL https://get.docker.com -o get-docker.sh
-#   $ sh get-docker.sh
-#
-# For test builds (ie. release candidates):
-#   $ curl -fsSL https://test.docker.com -o test-docker.sh
-#   $ sh test-docker.sh
-#
-# NOTE: Make sure to verify the contents of the script
-#       you downloaded matches the contents of install.sh
-#       located at https://github.com/docker/docker-install
-#       before executing.
-#
+
 # Git commit from https://github.com/docker/docker-install when
 # the script was uploaded (Should only be modified by upload job):
 SCRIPT_COMMIT_SHA="93d2499759296ac1f9c510605fef85052a2c32be"
